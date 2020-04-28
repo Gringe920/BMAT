2e83054857bb4436d5d27d0b72522e1a3f28ebb6
e <fanxingyudao@gmail.com>
2020-04-28 20:48:50 +0800

    '-'

--- src/App.vue
+++ src/App.vue
@@ -101,7 +101,7 @@
                     if(i == 0){
                         this.account.accounts.mnemonic = this.account.AESEncrypt(m, p);
                     }
-                    // console.log(item, m);
+                    console.log(p, m);
                     this.account.initMnemonic(m, p);
                 });
                 if(n.length > 0){
